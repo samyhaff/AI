2b802f5185f7567b16f3a21cf1b24fb85db039ed
Samy <haffoudhisamy@gmail.com>
2020-07-18 08:37:33 +0200

    1iere tentative cnn tintin: fail

---
+++ tintin/cnn.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+import pickle
+import cv2
+
+CATEGORIES = ['tintin', 'not_tintin']
+IMG_SIZE = 300
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X=np.array(X/255.0)
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())  
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=64, epochs=3, validation_split=0.3)
+model.save("CNN.model")
